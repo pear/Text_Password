d74e6cedb09e46f8b0254ce668e77e1bda83e7b6
Martin Jansen <mj@php.net>
2005-08-21 19:47:53 +0000

    * Added new header comment block.
    git-svn-id: http://svn.php.net/repository/pear/packages/Text_Password/trunk@194103 c90b9560-bf6c-de11-be94-00142212c4b1

--- Password.php
+++ Password.php
@@ -1,24 +1,26 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Martin Jansen <mj@php.net>                                  |
-// |          Olivier Vanhoucke <olivier@php.net>                         |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Class to create passwords
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Text
+ * @package    Text_Password
+ * @author     Martin Jansen <mj@php.net>
+ * @author     Olivier Vanhoucke <olivier@php.net>
+ * @copyright  2004-2005 Martin Jansen, Olivier Vanhoucke
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Text_Password
+ */
 
 /**
  * Number of possible characters in the password
@@ -26,11 +28,16 @@
 $_Text_Password_NumberOfPossibleCharacters = 0;
 
 /**
- * Create passwords
+ * Main class for the Text_Password package
  *
- * @package Text_Password
- * @author  Martin Jansen <mj@php.net>
- * @author  Olivier Vanhoucke <olivier@php.net>
+ * @category   Text
+ * @package    Text_Password
+ * @author     Martin Jansen <mj@php.net>
+ * @author     Olivier Vanhoucke <olivier@php.net>
+ * @copyright  2004-2005 Martin Jansen, Olivier Vanhoucke
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/Text_Password
  */
 class Text_Password {
 
