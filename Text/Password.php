3e846e58520afc1a0a9e56ce908ff8f413e7cc8d
Michael Gauthier <mike@silverorange.com>
2016-01-31 15:28:12 -0400

    Fix an instance of rand().
    Use self::_rand() instead.

--- Text/Password.php
+++ Text/Password.php
@@ -536,7 +536,7 @@ class Text_Password {
 
             // Get a random insertion position in the current password
             // value.
-            $randomPosition = rand(0, strlen($password) - 1);
+            $randomPosition = self::_rand(0, strlen($password) - 1);
 
             // Insert the new character in the current password value.
             $password = substr($password, 0, $randomPosition)
